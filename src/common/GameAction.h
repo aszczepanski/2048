045c596931d78e79038b375e7c1ac293c0e7f7d5
Adam Szczepański <aszczepanski92@gmail.com>
2015-07-30 19:33:36 +0200

    modified values of actions

--- src/common/GameAction.h
+++ src/common/GameAction.h
@@ -4,11 +4,14 @@
 #include <ostream>
 
 enum GameAction {
-	UP,
-	RIGHT,
-	DOWN,
-	LEFT
+	NO_ACTION = 0x0,
+	UP = 0x1,
+	RIGHT = 0x2,
+	DOWN = 0x4,
+	LEFT = 0x8
 };
+
+const GameAction gameActions[] = {UP, RIGHT, DOWN, LEFT};
 
 std::ostream& operator<<(std::ostream& out, const GameAction action);
 
