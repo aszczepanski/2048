012389e5ff3bd68df1aa911e02f015205bdb7bb4
Adam Szczepański <aszczepanski92@gmail.com>
2015-07-30 20:41:38 +0200

    improved game state methods and representation

--- src/common/GameState.cpp
+++ src/common/GameState.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -10,11 +11,23 @@
 using namespace std;
 
 const double GameState::TILE_2_PROBABILITY = 0.9;
+const double GameState::TILE_4_PROBABILITY = 0.1;
 
-void GameState::initialize(default_random_engine& randomEngine) {
-	stateInternal = 0;
-	addRandomTile(randomEngine);
-	addRandomTile(randomEngine);
+uint16_t GameState::rowLeftTable[65536];
+uint16_t GameState::rowRightTable[65536];
+uint64_t GameState::colUpTable[65536];
+uint64_t GameState::colDownTable[65536];
+
+int GameState::scoreTable[65536];
+
+unsigned GameState::initialize(default_random_engine& positionRandomEngine, default_random_engine& valueRandomEngine) {
+	clear();
+
+	unsigned scorePenalty = 0;
+	scorePenalty += addRandomTile(positionRandomEngine, valueRandomEngine);
+	scorePenalty += addRandomTile(positionRandomEngine, valueRandomEngine);
+
+	return scorePenalty;
 }
 
 bool GameState::isTerminalState() {
@@ -94,163 +107,39 @@ bool GameState::isActionEnabled(GameAction action) {
 	}
 }
 
-int GameState::computeAfterstate(GameAction action) {
-	assert(isActionEnabled(action));
-
-	int reward = 0;
-
+void GameState::computeAfterstate(GameAction action) {
 	if (action == LEFT) {
-		for (int i=0; i<4; i++) {
-			int nonZeros = 0;
-			int val = 0, nextVal = 0, newVal = 0;
-			for (int j=0; j<3; j++) {
-				val = getTileValue(i*4 + j);
-				if (val == 0) continue;
-
-				int jj = j;
-				for (; jj<3; jj++) {
-					nextVal = getTileValue(i*4 + jj + 1);
-					if (nextVal) {
-						jj++;
-						break;
-					}
-				}
-				if (val == nextVal) {
-					newVal = val + 1;
-					reward += (1<<newVal);
-					clearTileValue(i*4 + j);
-					clearTileValue(i*4 + jj);
-					setTileValue((i*4 + nonZeros++), newVal);
-				} else {
-					clearTileValue(i*4 + j);
-					clearTileValue(i*4 + jj);
-					setTileValue((i*4 + nonZeros++), val);
-					setTileValue((i*4 + nonZeros), nextVal);
-					j = nonZeros - 1;
-				}
-			}
-			if ((val = getTileValue(i*4 + 3)) > 0) {
-				clearTileValue(i*4 + 3);
-				setTileValue((i*4 + nonZeros), val);
-			}
-		}
+		stateInternal ^= uint64_t(rowLeftTable[(stateInternal >>  0) & UINT64_C(0xFFFF)]) <<  0;
+		stateInternal ^= uint64_t(rowLeftTable[(stateInternal >> 16) & UINT64_C(0xFFFF)]) << 16;
+		stateInternal ^= uint64_t(rowLeftTable[(stateInternal >> 32) & UINT64_C(0xFFFF)]) << 32;
+		stateInternal ^= uint64_t(rowLeftTable[(stateInternal >> 48) & UINT64_C(0xFFFF)]) << 48;
 	} else if (action == RIGHT) {
-		for (int i=0; i<4; i++) {
-			int nonZeros = 3;
-			int val = 0, nextVal = 0, newVal = 0;
-			for (int j=3; j>=1; j--) {
-				val = getTileValue(i*4 + j);
-				if (val == 0) continue;
-
-				int jj = j;
-				for (; jj>=1; jj--) {
-					nextVal = getTileValue(i*4 + (jj - 1));
-					if (nextVal) {
-						jj--;
-						break;
-					}
-				}
-				if (val == nextVal) {
-					newVal = val + 1;
-					reward += (1<<newVal);
-					clearTileValue(i*4 + j);
-					clearTileValue(i*4 + jj);
-					setTileValue((i*4 + nonZeros--), newVal);
-				} else {
-					clearTileValue(i*4 + j);
-					clearTileValue(i*4 + jj);
-					setTileValue((i*4 + nonZeros--), val);
-					setTileValue((i*4 + nonZeros), nextVal);
-					j = nonZeros + 1;
-				}
-			}
-			if ((val = getTileValue(i*4)) > 0) {
-				clearTileValue(i*4);
-				setTileValue((i*4 + nonZeros), val);
-			}
-		}
+		stateInternal ^= uint64_t(rowRightTable[(stateInternal >>  0) & UINT64_C(0xFFFF)]) <<  0;
+		stateInternal ^= uint64_t(rowRightTable[(stateInternal >> 16) & UINT64_C(0xFFFF)]) << 16;
+		stateInternal ^= uint64_t(rowRightTable[(stateInternal >> 32) & UINT64_C(0xFFFF)]) << 32;
+		stateInternal ^= uint64_t(rowRightTable[(stateInternal >> 48) & UINT64_C(0xFFFF)]) << 48;
 	} else if (action == UP) {
-		for (int i=0; i<4; i++) {
-			int nonZeros = 0;
-			int val = 0, nextVal = 0, newVal = 0;
-			for (int j=0; j<3; j++) {
-				val = getTileValue(i + j*4);
-				if (val == 0) continue;
-
-				int jj = j;
-				for (; jj<3; jj++) {
-					nextVal = getTileValue(i + (jj+1)*4);
-					if (nextVal) {
-						jj++;
-						break;
-					}
-				}
-				if (val == nextVal) {
-					newVal = val + 1;
-					reward += (1<<newVal);
-					clearTileValue(i + j*4);
-					clearTileValue(i + jj*4);
-					setTileValue((i + 4*nonZeros++), newVal);
-				} else {
-					clearTileValue(i + j*4);
-					clearTileValue(i + jj*4);
-					setTileValue((i + 4*nonZeros++), val);
-					setTileValue((i + 4*nonZeros), nextVal);
-					j = nonZeros - 1;
-				}
-			}
-			if ((val = getTileValue(i + 3*4)) > 0) {
-				clearTileValue(i + 3*4);
-				setTileValue((i + nonZeros*4), val);
-			}
-		}
+		uint64_t t = transpose(stateInternal);
+		stateInternal ^= colUpTable[(t >>  0) & UINT64_C(0xFFFF)] <<  0;
+		stateInternal ^= colUpTable[(t >> 16) & UINT64_C(0xFFFF)] <<  4;
+		stateInternal ^= colUpTable[(t >> 32) & UINT64_C(0xFFFF)] <<  8;
+		stateInternal ^= colUpTable[(t >> 48) & UINT64_C(0xFFFF)] << 12;
 	} else {
-		for (int i=0; i<4; i++) {
-			int nonZeros = 3;
-			int val = 0, nextVal = 0, newVal = 0;
-			for (int j=3; j>=1; j--) {
-				val = getTileValue(i + j*4);
-				if (val == 0) continue;
-
-				int jj = j;
-				for (; jj>=1; jj--) {
-					nextVal = getTileValue(i + (jj-1)*4);
-					if (nextVal) {
-						jj--;
-						break;
-					}
-				}
-				if (val == nextVal) {
-					newVal = val + 1;
-					reward += (1<<newVal);
-					clearTileValue(i + j*4);
-					clearTileValue(i + jj*4);
-					setTileValue((i + 4*nonZeros--), newVal);
-				} else {
-					clearTileValue(i + j*4);
-					clearTileValue(i + jj*4);
-					setTileValue((i + 4*nonZeros--), val);
-					setTileValue((i + 4*nonZeros), nextVal);
-					j = nonZeros + 1;
-				}
-			}
-			if ((val = getTileValue(i)) > 0) {
-				clearTileValue(i);
-				setTileValue((i + nonZeros*4), val);
-			}
-		}
+		uint64_t t = transpose(stateInternal);
+		stateInternal ^= colDownTable[(t >>  0) & UINT64_C(0xFFFF)] <<  0;
+		stateInternal ^= colDownTable[(t >> 16) & UINT64_C(0xFFFF)] <<  4;
+		stateInternal ^= colDownTable[(t >> 32) & UINT64_C(0xFFFF)] <<  8;
+		stateInternal ^= colDownTable[(t >> 48) & UINT64_C(0xFFFF)] << 12;
 	}
-
-	return reward;
 }
 
-void GameState::addRandomTile(default_random_engine& randomEngine) {
-	size_t randomTilePoint = getRandomTilePoint(randomEngine);
-	int randomTileValue = getRandomTileValue(randomEngine);
-
-	cout << randomTilePoint << " " << randomTileValue << endl;
+unsigned GameState::addRandomTile(default_random_engine& positionRandomEngine, default_random_engine& valueRandomEngine) {
+	size_t randomTilePoint = getRandomTilePoint(positionRandomEngine);
+	int randomTileValue = getRandomTileValue(valueRandomEngine);
 
 	setTileValue(randomTilePoint, randomTileValue);
+
+	return randomTileValue - 1;
 }
 
 size_t GameState::getRandomTilePoint(default_random_engine& randomEngine) {
@@ -281,4 +170,71 @@ void GameState::updateEmptyPoints(vector<size_t>& emptyPoints) {
 			emptyPoints.push_back(i);
 		}
 	}
+}
+
+int GameState::scoreBoard() {
+return scoreTable[(stateInternal >>  0) & UINT64_C(0xFFFF)]
+	+ scoreTable[(stateInternal >> 16) & UINT64_C(0xFFFF)]
+	+ scoreTable[(stateInternal >> 32) & UINT64_C(0xFFFF)]
+	+ scoreTable[(stateInternal >> 48) & UINT64_C(0xFFFF)];
+}
+
+void GameState::initializeTables() {
+	for (unsigned row=0; row<65536; row++) {
+		unsigned line[4] = {
+			(row >> 0) & 0xF,
+			(row >> 4) & 0xF,
+			(row >> 8) & 0xF,
+			(row >> 12) & 0xF
+		};
+
+		// score
+		int score = 0;
+		for (int i=0; i<4; i++) {
+			int rank = line[i];
+			if (rank >= 2) {
+				score += (rank-1) * (1<<rank);
+			}
+		}
+		scoreTable[row] = score;
+
+		// execute a move to the left
+		for (int i=0; i<3; i++) {
+			int j;
+			for (j=i+1; j<4; j++) {
+				if (line[j] != 0) break;
+			}
+			if (j == 4) break;
+
+			if (line[i] == 0) {
+				line[i] = line[j];
+				line[j] = 0;
+				i--;
+			} else if (line[i] == line[j]) {
+				if(line[i] != 0xf) {
+					line[i]++;  // 32768 + 32768 = 32768
+				}
+				line[j] = 0;
+			}
+		}
+
+		uint16_t result = (line[0]<<0) | (line[1]<<4) | (line[2]<<8) | (line[3]<<12);
+		uint16_t revResult = reverseRow(result);
+		unsigned revRow = reverseRow(row);
+
+		rowLeftTable[row] = row^result;
+		rowRightTable[revRow] = revRow^revResult;
+		colUpTable[row] = unpackCol(row)^unpackCol(result);
+		colDownTable[revRow] = unpackCol(revRow)^unpackCol(revResult);
+	}
+}
+
+uint16_t GameState::reverseRow(uint16_t row) {
+	return (row >> 12) | ((row >> 4) & 0x00F0)  | ((row << 4) & 0x0F00) | (row << 12);
+}
+
+uint64_t GameState::unpackCol(uint16_t row) {
+	uint64_t tmp = row;
+	return (tmp | (tmp << UINT64_C(12)) | (tmp << UINT64_C(24)) | (tmp << UINT64_C(36)))
+		& UINT64_C(0x000F000F000F000F);
 }
--- src/common/GameState.h
+++ src/common/GameState.h
@@ -9,11 +9,17 @@
 
 class GameState {
 public:
-	void initialize(std::default_random_engine&);
+	unsigned initialize(std::default_random_engine& positionRandomEngine,
+		std::default_random_engine& valueRandomEngine);  // returns a number of fours
 	bool isTerminalState();
 	bool isActionEnabled(GameAction);
-	int computeAfterstate(GameAction);  // returns a reward
-	void addRandomTile(std::default_random_engine&);
+	void computeAfterstate(GameAction);
+	unsigned addRandomTile(std::default_random_engine& positionRandomEngine,
+		std::default_random_engine& valueRandomEngine);  // returns a number of fours
+
+	void clear() {
+		stateInternal = UINT64_C(0);
+	}
 
 	std::size_t getRandomTilePoint(std::default_random_engine&);
 	int getRandomTileValue(std::default_random_engine&);
@@ -31,12 +37,46 @@ public:
 		stateInternal &= ~(UINT64_C(0xF) << (tile << 2));
 	}
 
+	int scoreBoard();
+
+	bool operator==(const GameState& gameState) {
+		return stateInternal == gameState.stateInternal;
+	}
+
+	operator uint64_t() const {
+		return stateInternal;
+	}
+
+	static const double TILE_2_PROBABILITY;
+	static const double TILE_4_PROBABILITY;
+
+	static void initializeTables();
+
 private:
 	void updateEmptyPoints(std::vector<std::size_t>& emptyPoints);
 
 	uint64_t stateInternal;
 
-	static const double TILE_2_PROBABILITY;
+	static uint16_t reverseRow(uint16_t row);
+	static uint64_t unpackCol(uint16_t row);
+
+	static uint64_t transpose(uint64_t board) {
+		uint64_t a1 = board & UINT64_C(0xF0F00F0FF0F00F0F);
+		uint64_t a2 = board & UINT64_C(0x0000F0F00000F0F0);
+		uint64_t a3 = board & UINT64_C(0x0F0F00000F0F0000);
+		uint64_t a = a1 | (a2 << 12) | (a3 >> 12);
+		uint64_t b1 = a & UINT64_C(0xFF00FF0000FF00FF);
+		uint64_t b2 = a & UINT64_C(0x00FF00FF00000000);
+		uint64_t b3 = a & UINT64_C(0x00000000FF00FF00);
+		return b1 | (b2 >> 24) | (b3 << 24);
+	}
+
+	static uint16_t rowLeftTable[65536];
+	static uint16_t rowRightTable[65536];
+	static uint64_t colUpTable[65536];
+	static uint64_t colDownTable[65536];
+
+	static int scoreTable[65536];
 };
 
 #endif  // SRC_COMMON_GAME_STATE_H_
