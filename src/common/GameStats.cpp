b20a96b46c57962fbdaf6b482704f5f2a97e0342
Adam Szczepański <aszczepanski92@gmail.com>
2015-08-04 22:00:23 +0200

    game stats gathering classes

---
+++ src/common/GameStats.cpp
@@ -0,0 +1,157 @@
+#include "common/GameStats.h"
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/median.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+
+#include "common/GameState.h"
+
+using namespace std;
+using namespace boost::accumulators;
+
+const uint8_t GameStats::gameStages[27][4] {
+	{15, 14, 13, 12},
+	{15, 14, 13},
+	{15, 14, 12},
+	{15, 14},
+	{15, 13, 12},
+	{15, 13},
+	{15, 13},
+	{15},
+	{14, 13, 12},
+	{14, 13},
+	{14, 12},
+	{14},
+	{13, 12},  // TODO: check the order
+	{13},
+	{12},
+	{11},
+	{10},
+	{9},
+	{8},
+	{7},
+	{6},
+	{5},
+	{4},
+	{3},
+	{2},
+	{1},
+	{0}
+};
+
+GameStats* GameStats::setScore(int score) {
+	this->score = score;
+	return this;
+}
+
+GameStats* GameStats::setMoves(int moves) {
+	this->moves = moves;
+	return this;
+}
+
+GameStats* GameStats::setDuration(DurationType duration) {
+	this->duration = duration;
+	return this;
+}
+
+GameStats* GameStats::setStage(size_t stage) {
+	this->stage = stage;
+	return this;
+}
+
+ostream& operator<<(ostream& out, const GameStats& gameStats) {
+	return out
+		<< gameStats.score
+		// << ", " << gameStats.moves
+		<< ", " << GameStats::stageToString(gameStats.stage)
+		<< ", " << gameStats.duration.count();
+}
+
+size_t GameStats::calculateStage(GameState gameState) {
+	array<int, 16> tiles;
+	for (size_t i=0; i<16; i++) {
+		tiles[i] = gameState.getTileValue(i);
+	}
+
+	sort(tiles.begin(), tiles.end(), greater<int>());
+
+	for (size_t i=0; i<27; i++) {
+		bool satisfied = true;
+		for (size_t j=0; j<4 && satisfied; j++) {
+			if (tiles[j] < gameStages[i][j]) satisfied = false;
+		}
+		if (satisfied) return i;
+	}
+
+	return 26;
+}
+
+string GameStats::stageToString(size_t stage) {
+	assert(stage < 27);
+
+	stringstream ss;
+	bool empty = true;
+	
+	for (int i=0; i<4; i++) {
+		if (gameStages[stage][i] == 0) break;
+		if (!empty) ss << "_";
+		if (gameStages[stage][i] >= 10) {
+			ss << (1<<(gameStages[stage][i]-10)) << "k";
+		} else {
+			ss << (1<<gameStages[stage][i]);
+		}
+		empty = false;
+	}
+
+	return ss.str();
+}
+
+void GameStatsContainer::addGameStats(const GameStats* gameStatsPtr) {
+	// TODO
+	scoreAccumulators(gameStatsPtr->score);
+	durationAccumulators(gameStatsPtr->duration.count());
+
+	for (int i=0; i<27; i++) {
+		stagesAccumulators[i](gameStatsPtr->stage <= i ? 1 : 0);
+	}
+}
+
+float GameStatsContainer::getScoreMean() const {
+	return mean(scoreAccumulators);
+}
+
+float GameStatsContainer::getScore95ConfidenceInterval() const {
+	return 1.96f * sqrt(variance(scoreAccumulators) / boost::accumulators::count(scoreAccumulators));
+}
+
+float GameStatsContainer::getStageCumulativeDistribution(size_t stage) const {
+	return mean(stagesAccumulators[stage]);
+}
+
+float GameStatsContainer::getDurationMean() const {
+	return mean(durationAccumulators);
+}
+
+ostream& operator<<(ostream& out, const GameStatsContainer& gameStatsContainer) {
+	out << gameStatsContainer.getScoreMean();
+	out	<< ", " << gameStatsContainer.getScore95ConfidenceInterval();
+
+	for (size_t i=0; i<16; i++) {
+		out << ", " << gameStatsContainer.getStageCumulativeDistribution(i);
+	}
+
+	out	<< ", " << gameStatsContainer.getDurationMean();
+
+	return out;
+}
---
+++ src/common/GameStats.h
@@ -0,0 +1,58 @@
+#ifndef SRC_COMMON_GAME_STATS_H_
+#define SRC_COMMON_GAME_STATS_H_
+
+#include <chrono>
+#include <cstdint>
+#include <ostream>
+#include <string>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/count.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/median.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+
+#include "common/GameState.h"
+
+class GameStats {
+public:
+	typedef std::chrono::duration<int, std::milli> DurationType;
+
+	GameStats* setScore(int score);
+	GameStats* setMoves(int moves);
+	GameStats* setDuration(DurationType duration);
+	GameStats* setStage(std::size_t);
+
+	friend std::ostream& operator<<(std::ostream&, const GameStats&);
+	friend class GameStatsHolder;
+
+// TODO: private:
+	int score;
+	int moves;
+	DurationType duration;
+	std::size_t stage;
+
+	static std::size_t calculateStage(GameState);
+	static std::string stageToString(std::size_t stage);
+	const static uint8_t gameStages[27][4];
+};
+
+class GameStatsContainer {
+public:
+	void addGameStats(const GameStats*);
+
+	float getScoreMean() const;
+	float getScore95ConfidenceInterval() const;
+	float getStageCumulativeDistribution(std::size_t stage) const;
+	float getDurationMean() const;
+
+	friend std::ostream& operator<<(std::ostream&, const GameStatsContainer&);
+
+private:
+	boost::accumulators::accumulator_set<int, boost::accumulators::stats<boost::accumulators::tag::count, boost::accumulators::tag::mean, boost::accumulators::tag::variance> > scoreAccumulators;
+	boost::accumulators::accumulator_set<int, boost::accumulators::stats<boost::accumulators::tag::mean> > durationAccumulators;
+	boost::accumulators::accumulator_set<int, boost::accumulators::stats<boost::accumulators::tag::mean> > stagesAccumulators[27];
+};
+
+#endif  // SRC_COMMON_GAME_STATS_H_
