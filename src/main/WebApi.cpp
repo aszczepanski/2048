87f4ee2f8a670a73985a6957ed8e89139e334c31
Adam Szczepański <aszczepanski92@gmail.com>
2015-12-13 13:35:00 +0100

    web api

---
+++ src/main/WebApi.cpp
@@ -0,0 +1,75 @@
+#include "main/WebApi.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "common/GameState.h"
+#include "common/GameStats.h"
+
+#include "common/TuplesDescriptor.h"
+#include "io/IInputFileReader.h"
+#include "io/BinaryInputFileReader.h"
+#include "io/TextInputFileReader.h"
+
+#include "Eval/IEvaluator.h"
+#include "eval/ExpectimaxEvaluator.h"
+#include "eval/IterativeExpectimaxEvaluator.h"
+#include "eval/RandomEvaluator.h"
+
+using namespace std;
+
+shared_ptr<ProgramOptions> programOptions;
+shared_ptr<TuplesDescriptor> tuplesDescriptor;
+shared_ptr<IEvaluator> evaluator;
+
+void initializeProgramOptions(const char* strategy) {
+	programOptions = make_shared<ProgramOptions>();
+
+	programOptions->strategy = string(strategy);
+	// programOptions->randomSeed = 123;
+	// programOptions->games = 1;
+	programOptions->maxRoundTime = 0;
+	programOptions->maxDepth = 3;
+	programOptions->threads = true;
+	// programOptions->verbose = false;
+}
+
+void loadTuplesDescriptor() {
+	shared_ptr<IInputFileReader> inputFileReader;
+	string fn = programOptions->strategy;
+	cout << "loading tuples from file " << fn << endl;
+  if(fn.substr(fn.find_last_of(".") + 1) == "txt") {
+    inputFileReader = make_shared<TextInputFileReader>();
+  } else {
+		inputFileReader = make_shared<BinaryInputFileReader>();
+  }
+
+	tuplesDescriptor = inputFileReader->read(programOptions->strategy);
+}
+
+void initializeTables() {
+	GameState::initializeTables();
+}
+
+void initializeEvaluator() {
+	// evaluator = make_shared<RandomEvaluator>(programOptions);
+	// TODO
+	if (programOptions->maxRoundTime > 0) {
+		evaluator = make_shared<IterativeExpectimaxEvaluator>(programOptions, tuplesDescriptor);
+	} else {
+		evaluator = make_shared<ExpectimaxEvaluator>(programOptions, tuplesDescriptor);
+	}
+	evaluator->reset();
+}
+
+int bestAction(GameState gameState) {
+	GameAction action = evaluator->bestAction(gameState);
+	switch (action) {
+	case UP: return ACTION_CODE_UP;
+	case DOWN: return ACTION_CODE_DOWN;
+	case LEFT: return ACTION_CODE_LEFT;
+	default: return ACTION_CODE_RIGHT;
+	}
+}
---
+++ src/main/WebApi.h
@@ -0,0 +1,51 @@
+#ifndef SRC_MAIN_WEB_API_H_
+#define SRC_MAIN_WEB_API_H_
+
+#include "common/GameState.h"
+
+// taken from: https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+	#ifdef BUILDING_DLL
+		#ifdef __GNUC__
+			#define DLL_PUBLIC __attribute__ ((dllexport))
+		#else
+			#define DLL_PUBLIC __declspec(dllexport)
+		#endif
+	#else
+		#ifdef __GNUC__
+			#define DLL_PUBLIC __attribute__ ((dllimport))
+		#else
+			#define DLL_PUBLIC __declspec(dllimport)
+		#endif
+	#endif
+	#define DLL_LOCAL
+	#else
+	#if __GNUC__ >= 4
+		#define DLL_PUBLIC __attribute__ ((visibility ("default")))
+		#define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+	#else
+		#define DLL_PUBLIC
+		#define DLL_LOCAL
+	#endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLL_PUBLIC void initializeProgramOptions(const char* strategy);
+DLL_PUBLIC void loadTuplesDescriptor();
+DLL_PUBLIC void initializeTables();
+DLL_PUBLIC void initializeEvaluator();
+DLL_PUBLIC int bestAction(GameState gameState);
+
+#ifdef __cplusplus
+}
+#endif
+
+const int ACTION_CODE_UP = 0;
+const int ACTION_CODE_DOWN = 1;
+const int ACTION_CODE_LEFT = 2;
+const int ACTION_CODE_RIGHT = 3;
+
+#endif  // SRC_MAIN_WEB_API_H_
